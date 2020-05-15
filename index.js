02e65bcef86a13af910c3fa47f6cd457896cf889
Ahmed Jamal <engahmedd96@gmai.com>
2020-05-15 15:46:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 function wakeDog(){
   
 }
+function wakeDog(){
+  
+}
+function wakeDog(){
+  
+}
+function wakeDog(){
+  
+}
+function wakeDog(){
+  
+}
+function wakeDog(){
+  
+}
+function wakeDog(){
+  
+}
+function wakeDog(){
+  
+}
+function wakeDog(){
+  
+}
+function wakeDog(){
+  
+}
